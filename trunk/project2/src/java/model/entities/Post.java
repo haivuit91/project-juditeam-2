8b969b6fd853749e548845911349f8b35d345800
tuannt.itedu@gmail.com <tuannt.itedu@gmail.com@be9f7bc6-0dc5-f015-6284-b55ef957619a>
2014-06-12 06:02:20 +0000

    [....] [add][ add entities]

---
+++ trunk/project2/src/java/model/entities/Post.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model.entities;
+
+import java.sql.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Tuanka
+ */
+public class Post {
+    
+    private int postID;
+    private String title;
+    private String shortTitle;
+    private String content;
+    private Date datePost;
+    private User user;
+    private Category category;
+    private boolean isActive;
+    
+    private List<Comment> commentList = null;
+
+    public Post() {
+        
+    }
+
+    public Post(int postID, String title, String shortTitle, String content, Date datePost, User user, Category category, boolean isActive) {
+        this.postID = postID;
+        this.title = title;
+        this.shortTitle = shortTitle;
+        this.content = content;
+        this.datePost = datePost;
+        this.user = user;
+        this.category = category;
+        this.isActive = isActive;
+    }
+
+    public int getPostID() {
+        return postID;
+    }
+
+    public void setPostID(int postID) {
+        this.postID = postID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getShortTitle() {
+        return shortTitle;
+    }
+
+    public void setShortTitle(String shortTitle) {
+        this.shortTitle = shortTitle;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDatePost() {
+        return datePost;
+    }
+
+    public void setDatePost(Date datePost) {
+        this.datePost = datePost;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public List<Comment> getCommentList() {
+        return commentList;
+    }
+
+    public void setCommentList(List<Comment> commentList) {
+        this.commentList = commentList;
+    }
+}
