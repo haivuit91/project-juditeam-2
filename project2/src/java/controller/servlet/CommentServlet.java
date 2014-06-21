65844c40ca04726fd15123b7da0e07f2ba9ddf94
tuannt.itedu <tuannt.itedu@gmail.com>
2014-06-21 09:27:10 +0000

    [] [update] [add key]

--- project2/src/java/controller/servlet/CommentServlet.java
+++ project2/src/java/controller/servlet/CommentServlet.java
@@ -6,8 +6,6 @@
 package controller.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -28,8 +26,9 @@ import util.Support;
  *
  * @author Tuanka
  */
-public class ViewPostServlet extends HttpServlet {
+public class CommentServlet extends HttpServlet {
 
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -41,15 +40,7 @@ public class ViewPostServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        String action = request.getParameter(Constants.ACTION);
-        if (action != null) {
-            switch (action) {
-                case "view":
-                    viewPost(request, response);
-                    break;
-            }
-        }
+
     }
 
     /**
@@ -63,29 +54,36 @@ public class ViewPostServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        String action = request.getParameter(Constants.ACTION);
-       
+        String action = request.getParameter("action");
+        if (action != null) {
+            switch (action) {
+                case "comment":
+                    addComment(request, response);
+                    break;
+            }
+        }
     }
 
-   
+    private void addComment(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PostDAOService postService = PostDAO.getInstance();
+        CommentDAOService commentService = CommentDAO.getInstance();
+        String content = request.getParameter("content");
+        int postID = Integer.valueOf(request.getParameter("id"));
+        Post post = postService.getPostByID(postID);
+        User user = (User) request.getSession().getAttribute(Constants.CURRENT_USER);
+        Comment comment = new Comment(0, content, Support.getDatePost(), post, user, true);
+        if (commentService.insertComment(comment)) {
+            request.setAttribute(Constants.MSG_RESULT, "Bình luận của bạn đã đươc gửi");
+        } else {
+            request.setAttribute(Constants.MSG_RESULT, "Không thể gửi bình luận");
+        }
+        refreshViewPost(request, response, postID);
+    }
+
     private void refreshViewPost(HttpServletRequest request, HttpServletResponse response, int postID) throws ServletException, IOException {
         PostDAOService postService = PostDAO.getInstance();
         CommentDAOService commentService = CommentDAO.getInstance();
         AttachDAOService attachService = AttachDAO.getInstance();
-        Post post = postService.getPostByID(postID);
-        post.setCommentList(commentService.getCommentByPostID(postID));
-        post.setAttachList(attachService.getAttachByPostID(postID));
-        request.setAttribute(Constants.CURRENT_POST, post);
-        request.setAttribute(Constants.PAGE, "view-post");
-        request.getRequestDispatcher(Constants.URL_HOME).forward(request, response);
-    }
-
-    private void viewPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PostDAOService postService = PostDAO.getInstance();
-        CommentDAOService commentService = CommentDAO.getInstance();
-        AttachDAOService attachService = AttachDAO.getInstance();
-        int postID = Integer.valueOf(request.getParameter("id"));
         Post post = postService.getPostByID(postID);
         post.setCommentList(commentService.getCommentByPostID(postID));
         post.setAttachList(attachService.getAttachByPostID(postID));
