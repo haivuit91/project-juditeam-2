0eab7f24c52d4857567fac134df9211fbf550b84
haivv.itedu <haivv.itedu@gmail.com>
2014-06-22 14:12:32 +0000

    [setting ckeditor]

--- project2/web/ckeditor/config.js
+++ project2/web/ckeditor/config.js
@@ -9,18 +9,17 @@
 
             config.toolbar_MyToolbar =
                     [
+                        {name: 'basicstyles', items: ['RemoveFormat']},
                         {name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']},
-                        {name: 'editing', items: ['Find', 'Replace', '-', 'SelectAll', '-', 'SpellChecker', 'Scayt']},
-                        {name: 'forms', items: ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton',
-                                'HiddenField']},
+                        {name: 'editing', items: ['Find', 'Replace', '-', 'SelectAll']},
+                        {name: 'insert', items: ['Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe']},
                         '/',
-                        {name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']},
-                        {name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv',
+                        {name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Subscript', 'Superscript', '-']},
+                        {name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent',
                                 '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl']},
                         {name: 'links', items: ['Link', 'Unlink', 'Anchor']},
                         '/',
                         {name: 'styles', items: ['Styles', 'Format', 'Font', 'FontSize']},
                         {name: 'colors', items: ['TextColor', 'BGColor']},
-                        {name: 'insert', items: ['Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe']},
                     ];
         };
